4dc4eb1d29978d0a1ed0c46d8ddb4d247cf3bfff
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2021-09-21 22:04:50 +0100

    finish off submitting events

--- crunchy/commands/events.py
+++ crunchy/commands/events.py
@@ -1,4 +1,5 @@
 import pprint
+from enum import Enum
 
 from roid import CommandsBlueprint, Response, ResponseFlags, Interaction
 from roid.objects import Channel, ChannelType
@@ -14,6 +15,13 @@ NOT_ENOUGH_DATA = AbortInvoke(
     content="<:HimeSad:676087829557936149> Oops! You've not given me enough information to work with here.",
     flags=ResponseFlags.EPHEMERAL,
 )
+
+
+class EventType(Enum):
+    """The type of event to register with the API."""
+
+    News = "news"
+    Releases = "releases"
 
 
 events_blueprint = CommandsBlueprint()
@@ -58,14 +66,39 @@ async def check_channel_type(interaction: Interaction):
     return interaction
 
 
-async def get_webhook_url(app: CommandHandler, channel: Channel, sub_type: str) -> str:
+async def get_webhook_url(
+    app: CommandHandler,
+    channel: Channel,
+    sub_type: EventType,
+) -> str:
+    """
+    Creates a webhook in the given channel and returns the computed URL
+    from the response.
+
+    Any issue with the request is returned with a roid.exception's HTTPException
+    subclass.
+
+    Args:
+        app:
+            The slash commands app which has a http handler.
+
+        channel:
+            The channel to target the webhook creation.
+
+        sub_type:
+            Which event type the webhook belongs to (news or releases).
+
+    Returns:
+        A formatted webhook execution url.
+    """
+
     avatar = assetloader.get_base64_asset("crunchy-128.webp")
     data = await app.http.request(
         "POST",
         f"/channels/{channel.id}/webhooks",
         pass_token=True,
         json={
-            "name": f"Crunchy Anime {sub_type}",
+            "name": f"Crunchy Anime {sub_type.name}",
             "avatar": f"data:image/webp;base64,{avatar}",
         },
     )
@@ -74,14 +107,34 @@ async def get_webhook_url(app: CommandHandler, channel: Channel, sub_type: str) 
 
 
 async def submit_webhook(
-    app: CommandHandler, target: str, guild_id: int, webhook_url: str
+    app: CommandHandler,
+    sub_type: EventType,
+    guild_id: int,
+    webhook_url: str,
 ):
+    """
+    Submits a webhook to the Crunchy API.
+
+    Args:
+        app:
+            The slash commands app with the client attribute linking to the CrunchyApi
+            handler.
+
+        sub_type:
+            Which event type the webhook belongs to (news or releases).
+
+        guild_id:
+            The id of the guild the webhook belongs to.
+
+        webhook_url:
+            The url of the webhook.
+    """
     payload = {
         "guild_id": str(guild_id),
         "webhook_url": webhook_url,
     }
 
-    await app.client.request("POST", f"/events/{target}/update", json=payload)
+    await app.client.request("POST", f"/events/{sub_type.value}/update", json=payload)
 
 
 @check(check_channel_type)
@@ -94,13 +147,19 @@ async def submit_webhook(
     defer_register=False,
 )
 async def add_news_channel(
-    app: CommandHandler, interaction: Interaction, channel: Channel
+    app: CommandHandler,
+    interaction: Interaction,
+    channel: Channel,
 ) -> Response:
-    url = await get_webhook_url(app, channel, "News")
+    url = await get_webhook_url(
+        app=app,
+        channel=channel,
+        sub_type=EventType.News,
+    )
 
     await submit_webhook(
         app=app,
-        target="news",
+        sub_type=EventType.News,
         guild_id=interaction.guild_id,
         webhook_url=url,
     )
@@ -124,13 +183,19 @@ async def add_news_channel(
     defer_register=False,
 )
 async def add_release_channel(
-    app: CommandHandler, interaction: Interaction, channel: Channel
+    app: CommandHandler,
+    interaction: Interaction,
+    channel: Channel,
 ) -> Response:
-    url = await get_webhook_url(app, channel, "Releases")
+    url = await get_webhook_url(
+        app=app,
+        channel=channel,
+        sub_type=EventType.Releases,
+    )
 
     await submit_webhook(
         app=app,
-        target="releases",
+        sub_type=EventType.Releases,
         guild_id=interaction.guild_id,
         webhook_url=url,
     )
