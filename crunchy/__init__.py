ba140abd15c69a6a3ec991829e66dcb70b4c28a5
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2021-09-21 19:20:36 +0100

    add redis

--- crunchy/__init__.py
+++ crunchy/__init__.py
@@ -1,7 +1,4 @@
-__version__ = '0.1.0'
+__version__ = "0.1.0"
 
-from .main import main
-
-from dotenv import load_dotenv
-
-load_dotenv()
+from . import env
+from .main import main, app
--- crunchy/main.py
+++ crunchy/main.py
@@ -2,18 +2,24 @@ import os
 import uvicorn
 
 from roid import SlashCommands
+from roid.state import RedisBackend
 
 
 app = SlashCommands(
     application_id=int(os.getenv("APPLICATION_ID")),
     application_public_key=os.getenv("PUBLIC_KEY"),
     token=os.getenv("BOT_TOKEN"),
+    state_backend=RedisBackend(
+        host=os.getenv("REDIS_HOST", "127.0.0.1"),
+        port=int(os.getenv("REDIS_PORT", 6379)),
+        db=int(os.getenv("REDIS_DB", 0)),
+    ),
 )
 
 
 def main():
-    ...
+    uvicorn.run("crunchy:app")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
